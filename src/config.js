2df4c53a81799cb9ae15387d17a0bc475ad890ab
Zack Bunn <zrb6578@gmail.com>
2019-04-27 15:48:14 -0500

    deployed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://sheltered-harbor-11044.herokuapp.com'
-    //API_ENDPOINT: 'http://localhost:8000'
+    //API_ENDPOINT: 'https://sheltered-harbor-11044.herokuapp.com'
+    API_ENDPOINT: 'http://localhost:8000'
 }
