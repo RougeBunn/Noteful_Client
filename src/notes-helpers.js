cd4770e172ae46879e7ef7b57eb5c27fd95f7f1c
Zack Bunn <zrb6578@gmail.com>
2019-04-27 22:03:21 -0500

    fixed errors

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -7,7 +7,7 @@ export const findNote = (notes=[], note_id) =>
 export const getNotesForFolder = (notes=[], folder_id) => (
   (!folder_id)
     ? notes
-    : notes.filter(note => note.folder_id === folder_id)
+    : notes.filter(note => note.folder_id == folder_id)
 )
 
 export const countNotesForFolder = (notes=[], folder_id) =>
