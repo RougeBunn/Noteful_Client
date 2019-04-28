1cc60730b220c801523011f3a331e5f33d058d96
Zack Bunn <zrb6578@gmail.com>
2019-04-28 17:03:50 -0500

    fixed another error

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -2,7 +2,7 @@ export const findFolder = (folders=[], folder_id) =>
   folders.find(folder => folder.id === folder_id)
 
 export const findNote = (notes=[], note_id) =>
-  notes.find(note => note.id === note_id)
+  notes.find(note => note.id == note_id)
 
 export const getNotesForFolder = (notes=[], folder_id) => (
   (!folder_id)
